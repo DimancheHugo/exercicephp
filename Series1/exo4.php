c17b2520caac348ce2a08c5604488cce3a4bad62
Hugo Dimanche <hugo.dimanche@lycee-jeanrostand.fr>
2024-10-10 10:44:02 +0200

    quatrième exercice

---
+++ Series1/exo4.php
@@ -0,0 +1,9 @@
+<?php
+    $argument1="Antoine";
+    $argument2="Griezmann";
+    function concatenation($argument1,$argument2){
+        return $argument1.$argument2;
+
+    }
+echo concatenation($argument1,$argument2);
+?>
