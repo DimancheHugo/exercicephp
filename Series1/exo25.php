095d3b8b558339224b76871ac1776e560c3167af
Hugo Dimanche <hugo.dimanche@lycee-jeanrostand.fr>
2024-10-17 11:34:22 +0200

    exercice 25

---
+++ Series1/exo25.php
@@ -0,0 +1,24 @@
+<?php
+
+include './libraryToInclude.php';
+
+function getUtilisateursAutorises() {
+
+    $utilisateurs = getAllUtilisateurs();
+
+    $utilisateursAutorises = array();
+
+    foreach ($utilisateurs as $utilisateur) {
+
+        if (empty($utilisateur->blocked) && !empty($utilisateur->email) && $utilisateur->age >= 18) {
+
+            $utilisateursAutorises[] = $utilisateur;
+        }
+    }
+
+    return $utilisateursAutorises;
+}
+
+$utilisateursAutorises = getUtilisateursAutorises();
+print_r($utilisateursAutorises);
+?>
