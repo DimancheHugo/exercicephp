b56211f27bcbfed93131991608b007163202336f
Hugo Dimanche <hugo.dimanche@lycee-jeanrostand.fr>
2024-10-17 12:27:44 +0200

    Exercice13

---
+++ Series1/site2/Exercice13.php
@@ -0,0 +1,6 @@
+<?php
+$pays = array('france'=>'paris','Allemagne'=>'Berlin','Italie'=>'Rome');
+foreach($pays as $cle => $valeur) {
+    echo 'La capitale de '.$cle.' est '.$valeur.'<br />';
+}
+?>
