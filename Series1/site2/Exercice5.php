f776cf3a2036e41f56f376edf8c67669e07abd72
Hugo Dimanche <hugo.dimanche@lycee-jeanrostand.fr>
2024-10-17 12:07:28 +0200

    Exercice5

---
+++ Series1/site2/Exercice5.php
@@ -0,0 +1,10 @@
+<?php
+$sexe = "hélicoptère de combat";
+if ($sexe == "homme") {
+    echo "Bonjour Monsieur";
+} elseif ($sexe == "femme") {
+    echo "Bonjour Madame";
+}else{
+    echo "Bonjour, vous n'existez pas";
+}
+?>
